d3fd5eb5e4d432fad443d593aa63e2acb44b0292
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2018-06-27 18:29:58 +0200

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,37 @@
 import * as assert from 'assert'
 import is from '@sindresorhus/is'
 
-export type Middleware<T> = (context: T, next: () => any) => any;
+export type Middleware<T> = (input: T, next: () => any) => any;
 
-export type ComposedMiddleware<T> = (context: T, done?: () => any) => Promise<any>;
+export type ComposedMiddleware<T> = (input: T, done?: () => any) => Promise<any>;
 
 /**
  * Compose the given middlewares
  * 
  * @param fns An array of middleware functions
+ * @throws `TypeError` if the argument is not an array of functions
  * @public
  */
 export function compose<T> (fns: Middleware<T>[]): ComposedMiddleware<T> {
-  assert(is.array(fns), `Expect an array of functions but got "${is(fns)}"`)
-
-  for (let fn of fns) {
-    assert(is.function_(fn), `Expect a function but got "${is(fn)}"`)
+  if (is.array(fns)) {
+    throw new TypeError(`Expect an array but got "${is(fns)}"`)
   }
 
-  return (input: T, done?: () => any) => {
-    var i = 0
+  for (let fn of fns) {
+    throw new TypeError(`Expect a function but got "${is(fn)}"`)
+  }
+
+  return async (input: T, done?: () => any) => {
+    let i = 0
 
     function next (): any {
-      var fn = fns[i++]
+      let fn = fns[i++]
 
       if (fn) return fn(input, next)
 
       if (done) return done()
     }
 
-    return new Promise((r) => r(next()))
+    return next()
   }
 }
